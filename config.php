b70db7087353f5c4a4907327519aec191b27561d
Tweedyzi <30966798+Tweedyzi@users.noreply.github.com>
2025-06-26 22:27:38 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+
+<?php
+// Clé API OpenAI sécurisée
+define('OPENAI_API_KEY', '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');
+?>
